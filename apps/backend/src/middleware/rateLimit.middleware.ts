17801425d8989531811aca435daa95b0bfbda69a
Andy <linandy40804@gmail.com>
2025-09-13 00:15:37 +0800

    [fix] relax rate limit restrictions

--- apps/backend/src/middleware/rateLimit.middleware.ts
+++ apps/backend/src/middleware/rateLimit.middleware.ts
@@ -1,8 +1,8 @@
 import rateLimit from "express-rate-limit";
 
 export const basicLimiter = rateLimit({
-  windowMs: 5 * 60 * 1000, // 15 minutes
-  max: 35, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  windowMs: 5 * 60 * 1000, // 5 minutes
+  max: 350, // Limit each IP to 100 requests per `window` (here, per 5 minutes)
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
   message: {
