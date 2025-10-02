f028015487277bb14762588a5a3265e1c5c81ce0
Andy <linandy40804@gmail.com>
2025-10-02 23:29:48 +0800

    [feat] tranfer schools functionalities to nestjs ver

--- apps/backend-nestjs/src/schools/schools.service.ts
+++ apps/backend-nestjs/src/schools/schools.service.ts
@@ -1,7 +1,59 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma, School } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { SchoolDTO } from './dto/schools.dto';
+import { BadRequestError, NotFoundError } from 'src/types/error.types';
 
 @Injectable()
 export class SchoolsService {
   constructor(private readonly prisma: PrismaService) {}
+
+  async getAvailableSchools(
+    provider?: 'google' | 'credential',
+  ): Promise<SchoolDTO[]> {
+    let schools: School[] = [];
+    if (!provider) {
+      schools = await this.prisma.school.findMany();
+    } else if (provider === 'google') {
+      schools = await this.prisma.school.findMany({
+        where: {
+          emailFormats: {
+            isEmpty: false,
+          },
+        },
+      });
+    } else if (provider === 'credential') {
+      schools = await this.prisma.school.findMany({
+        where: {
+          studentIdFormat: {
+            not: Prisma.JsonNull,
+          },
+        },
+      });
+    } else {
+      throw new BadRequestError('Invalid login type');
+    }
+
+    return schools.map((school) => ({
+      id: school.id,
+      name: school.name,
+      abbreviation: school.abbreviation,
+      loginMethod: school.emailFormats ? 'google' : 'credential',
+    }));
+  }
+
+  async getSchoolById(id: string): Promise<SchoolDTO> {
+    const school = await this.prisma.school.findUnique({
+      where: { id },
+    });
+
+    if (!school) throw new NotFoundError('SCHOOL');
+
+    return {
+      id: school.id,
+      name: school.name,
+      abbreviation: school.abbreviation,
+      loginMethod: school.studentIdFormat ? 'credential' : 'google',
+    };
+  }
 }
