d6c28c9d6edc37757d0ec82c0c08da2b8c898194
Andy <linandy40804@gmail.com>
2025-10-13 23:02:32 +0800

    [fix] use `a` rather than `button` for login button

--- apps/frontend/src/widgets/LoginButton.tsx
+++ apps/frontend/src/widgets/LoginButton.tsx
@@ -1,15 +1,8 @@
-import { useNavigate } from "react-router-dom";
-
 const LoginButton = () => {
-  const navigate = useNavigate();
-  const toLoginPage = () => {
-    navigate("/choose-school");
-  };
-
   return (
-    <button onClick={toLoginPage} className="btn btn-gost">
+    <a href="/choose-school" className="btn btn-gost">
       登入
-    </button>
+    </a>
   );
 };
 
