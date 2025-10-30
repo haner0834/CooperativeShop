37b0877382330a7cabb899cf0021517289a2dd19
Andy <linandy40804@gmail.com>
2025-10-31 00:35:27 +0800

    [feat] optimize the adaptation for shop cell size

--- apps/frontend/src/pages/Shops.tsx
+++ apps/frontend/src/pages/Shops.tsx
@@ -243,7 +243,7 @@ const Shops = () => {
         </section>
         <section className="">
           <SectionTitle title="All Shops" />
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 px-4 space-y-2">
+          <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 px-4 space-y-2">
             {testShops.map((shop) => (
               <ShopCard shop={shop} className="w-full" />
             ))}
