25c801d8b105e07de53c5c09b28af7aefa27459a
Andy <linandy40804@gmail.com>
2025-10-31 23:15:22 +0800

    [feat] add textfield animation

--- apps/frontend/src/pages/Shops.tsx
+++ apps/frontend/src/pages/Shops.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import {
   Bookmark,
   ChevronRight,
@@ -8,7 +8,9 @@ import {
   Phone,
   School,
   Search,
+  X,
 } from "lucide-react";
+import { motion, AnimatePresence } from "framer-motion";
 import Sidebar from "../widgets/Sidebar";
 import Logo from "@shared/app-icons/cooperativeshop-logo.svg?react";
 import ThemeToggle from "../widgets/ThemeToggle";
@@ -148,12 +150,17 @@ const ShopCard = ({ shop, className }: { shop: Shop; className: string }) => {
   );
 };
 
+const transitionProps = {
+  type: "tween",
+  duration: 0.2,
+} as const;
+
 const Shops = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const [search, setSearch] = useState("");
+  const [isSearchFocused, setIsSearchFocused] = useState(false);
 
   const closeSidebar = () => setIsSidebarOpen(false);
-  const openSidebar = () => setIsSidebarOpen(true);
   const toggleSidebar = () => setIsSidebarOpen((prev) => !prev);
 
   return (
@@ -191,33 +198,135 @@ const Shops = () => {
         </div>
       </Sidebar>
 
-      <div className="navbar bg-base-100 shadow-sm z-50 fixed">
-        <div className="navbar-start">
+      <div className="navbar bg-base-100 shadow-sm z-50 fixed overflow-hidden">
+        <AnimatePresence initial={false}>
+          {!isSearchFocused ? (
+            <>
+              <motion.div
+                className="navbar-start"
+                key="navbar-start"
+                initial={{ opacity: 0, scale: 0.95 }}
+                animate={{ opacity: 1, scale: 1 }}
+                exit={{
+                  opacity: 0,
+                  scale: 0.95,
+                  position: "absolute",
+                  left: 0,
+                  top: 0,
+                  height: "100%",
+                  width: "33.33%", // daisyUI navbar-start 默認是 flex: 1
+                }}
+                transition={transitionProps}
+              >
+                <button
+                  className="btn btn-ghost btn-square lg:hidden"
+                  onClick={toggleSidebar}
+                >
+                  <Menu />
+                </button>
+                <Logo className="h-10 w-auto hidden lg:block" />
+              </motion.div>
+
+              <motion.div
+                className="navbar-center"
+                key="navbar-center"
+                initial={{ opacity: 0, scale: 0.95 }}
+                animate={{ opacity: 1, scale: 1 }}
+                exit={{
+                  opacity: 0,
+                  scale: 0.95,
+                  position: "absolute",
+                  left: "33.33%", // 讓它從中間開始 fade out
+                  top: 0,
+                  height: "100%",
+                  width: "33.33%", // daisyUI navbar-center 默認是 flex: 1
+                }}
+                transition={transitionProps}
+              >
+                <Logo className="h-10 w-auto lg:hidden" />
+
+                <div className="hidden lg:block">
+                  <label className="input w-[400px] flex items-center gap-2">
+                    <Search className="opacity-50 w-5 h-5" />
+                    <input
+                      type="search"
+                      value={search}
+                      onChange={(e) => setSearch(e.target.value)}
+                      required
+                      placeholder="Search"
+                    />
+                    <kbd className="kbd kbd-sm rounded-sm opacity-50">⌘ K</kbd>
+                  </label>
+                </div>
+              </motion.div>
+            </>
+          ) : (
+            <motion.div
+              className="flex-1"
+              key="search-bar"
+              initial={{ opacity: 0, scale: 0.95 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{
+                opacity: 0,
+                scale: 0.95,
+                position: "absolute",
+                top: "18%", // These two magic numbers for fight against the offset after end animation triggered
+                left: "2%",
+                height: "100%",
+                width: "100%", // 讓它在離開時佔據整個寬度
+              }}
+              transition={transitionProps}
+            >
+              {/* 10 px for avoiding X icon */}
+              <div className="flex h-full pr-[10px]">
+                <label className="input w-full flex items-center gap-2">
+                  <Search className="opacity-50 w-5 h-5" />
+                  <input
+                    type="search"
+                    required
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                    placeholder="Search"
+                    className="grow"
+                  />
+                </label>
+              </div>
+            </motion.div>
+          )}
+        </AnimatePresence>
+
+        <div
+          className={
+            isSearchFocused ? "z-10 relative" : "navbar-end z-10 relative"
+          }
+        >
           <button
-            className="btn btn-ghost btn-square lg:hidden"
-            onClick={toggleSidebar}
+            className="btn btn-ghost btn-circle lg:hidden"
+            onClick={() => setIsSearchFocused((prev) => !prev)}
           >
-            <Menu />
-          </button>
-          <Logo className="h-10 w-auto hidden lg:block" />
-        </div>
-
-        <div className="navbar-center">
-          <Logo className="h-10 w-auto lg:hidden" />
-
-          <div className="hidden lg:block">
-            {/* IDK why but it works */}
-            <label className="input w-[400px]">
-              <Search className="opacity-50 w-5 h-5" />
-              <input type="search" required placeholder="Search" />
-              <kbd className="kbd kbd-sm rounded-sm opacity-50">⌘ K</kbd>
-            </label>
-          </div>
-        </div>
-
-        <div className="navbar-end">
-          <button className="btn btn-ghost btn-circle lg:hidden">
-            <Search />
+            <AnimatePresence>
+              {isSearchFocused ? (
+                <motion.div
+                  key="close-icon"
+                  initial={{ opacity: 0, scale: 0.5 }}
+                  animate={{ opacity: 1, scale: 1 }}
+                  exit={{ opacity: 0, scale: 0.5, position: "absolute" }}
+                  transition={transitionProps}
+                >
+                  <X />
+                </motion.div>
+              ) : (
+                <motion.div
+                  key="search-icon"
+                  initial={{ opacity: 0, scale: 0.5 }}
+                  animate={{ opacity: 1, scale: 1 }}
+                  exit={{ opacity: 0, scale: 0.5, position: "absolute" }}
+                  transition={transitionProps}
+                >
+                  <Search />
+                </motion.div>
+              )}
+            </AnimatePresence>
           </button>
 
           <div className="hidden lg:block">
@@ -231,7 +340,7 @@ const Shops = () => {
           <SectionTitle title="Recent Visited" />
           <div className="overflow-x-scroll whitespace-normal px-4">
             <div className="inline-flex space-x-4">
-              {[...testShops, ...testShops].map((shop) => (
+              {[...testShops].map((shop) => (
                 <ShopCard
                   key={"recent-" + shop.id}
                   shop={shop}
@@ -245,7 +354,7 @@ const Shops = () => {
           <SectionTitle title="All Shops" />
           <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 px-4 space-y-2">
             {testShops.map((shop) => (
-              <ShopCard shop={shop} className="w-full" />
+              <ShopCard key={shop.id} shop={shop} className="w-full" />
             ))}
           </div>
         </section>
