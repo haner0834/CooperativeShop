30099b5b2e98ad7af739438aa433114d256ce596
Andy <linandy40804@gmail.com>
2025-11-09 20:42:08 +0800

    [hotfix] fix AuthModule dependency issue on StorageModule

--- apps/backend/src/storage/storage.module.ts
+++ apps/backend/src/storage/storage.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { StorageService } from './storage.service';
 import { StorageController } from './storage.controller';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   controllers: [StorageController],
   providers: [StorageService],
+  imports: [AuthModule],
 })
 export class StorageModule {}
