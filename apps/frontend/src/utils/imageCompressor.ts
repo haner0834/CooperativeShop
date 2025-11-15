1bdc3f0be9465a24a4f2794d6d72251032c99472
Andy <linandy40804@gmail.com>
2025-11-15 16:53:19 +0800

    [feat] align the maxSize in image compressor to the backend

--- apps/frontend/src/utils/imageCompressor.ts
+++ apps/frontend/src/utils/imageCompressor.ts
@@ -4,11 +4,13 @@ export async function compressImage(file: File) {
   const mainImage = await imageCompression(file, {
     maxWidthOrHeight: 1280, // 主圖最大邊
     useWebWorker: true,
+    maxSizeMB: 1,
   });
 
   const thumbnail = await imageCompression(file, {
     maxWidthOrHeight: 700,
     useWebWorker: true,
+    maxSizeMB: 0.015,
   });
 
   return { mainImage, thumbnail };
