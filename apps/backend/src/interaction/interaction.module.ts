3c62c7d794a74611b74e541e2270229155b0a406
Andy <linandy40804@gmail.com>
2025-12-01 21:29:29 +0800

    [fix] fix dependency

--- apps/backend/src/interaction/interaction.module.ts
+++ apps/backend/src/interaction/interaction.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { InteractionService } from './interaction.service';
 import { InteractionController } from './interaction.controller';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   controllers: [InteractionController],
   providers: [InteractionService],
+  imports: [AuthModule],
 })
 export class InteractionModule {}
