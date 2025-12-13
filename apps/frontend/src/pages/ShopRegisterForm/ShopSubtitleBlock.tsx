c8eea0aeb9bb6495c151777642e330791d587d56
Andy <linandy40804@gmail.com>
2025-12-14 01:14:44 +0800

    [feat] improve questions description

--- apps/frontend/src/pages/ShopRegisterForm/ShopSubtitleBlock.tsx
+++ apps/frontend/src/pages/ShopRegisterForm/ShopSubtitleBlock.tsx
@@ -9,12 +9,16 @@ const ShopSubtitleBlock = ({
   setSubtitle: Dispatch<React.SetStateAction<string>>;
 }) => {
   return (
-    <QuestionBlock title="副標題" status="optional">
+    <QuestionBlock
+      title="副標題"
+      description="請填寫該店家的 **分店名或地區** ，如：崇德店"
+      status="optional"
+    >
       <input
         type="text"
         value={subTitle}
         onChange={(e) => setSubtitle(e.target.value)}
-        placeholder="台南店"
+        placeholder="崇德店"
         className="input w-full"
       />
     </QuestionBlock>
