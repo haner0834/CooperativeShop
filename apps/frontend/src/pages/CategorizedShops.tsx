50077086f51e0c3e74e61e7ee5c3df4ea1fbc684
Andy <linandy40804@gmail.com>
2025-12-16 23:11:25 +0800

    [feat] add a view when shops is empty

--- apps/frontend/src/pages/CategorizedShops.tsx
+++ apps/frontend/src/pages/CategorizedShops.tsx
@@ -1,4 +1,4 @@
-import { useParams, useSearchParams } from "react-router-dom";
+import { Link, useParams, useSearchParams } from "react-router-dom";
 import ShopCard from "../widgets/Shop/ShopCard";
 import { useEffect, useState } from "react";
 import { useModal } from "../widgets/ModalContext";
@@ -8,7 +8,7 @@ import { getErrorMessage } from "../utils/errors";
 import { transformDtoToShop, type Shop } from "../types/shop";
 import BackButton from "../widgets/BackButton";
 import ThemeToggle from "../widgets/ThemeToggle";
-import { Trash2 } from "lucide-react";
+import { ShoppingCart, Trash2 } from "lucide-react";
 import { useAuthFetch } from "../auth/useAuthFetch";
 
 type ShopFilter =
@@ -121,6 +121,21 @@ const FilteredShops = () => {
         </div>
       </div>
       <main className="pt-16">
+        {shops.length === 0 && (
+          <div className="min-h-screen relative flex flex-col justify-center items-center space-y-4">
+            <div className="flex space-x-2">
+              <ShoppingCart />
+              <h2>尚無商家</h2>
+            </div>
+            <Link
+              to="/shops/drafts"
+              className="btn btn-primary btn-wide rounded-full"
+            >
+              建立商家草稿
+            </Link>
+            <ShoppingCart className="w-100 h-100 rotate-36 -z-10 opacity-5 absolute" />
+          </div>
+        )}
         <section className="pt-4">
           <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 px-4 space-y-2">
             {shops.map((shop) => (
