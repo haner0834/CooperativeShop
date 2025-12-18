a0c1eea93c452b9fbde69346f598a69793c36d26
Andy <linandy40804@gmail.com>
2025-12-18 23:33:18 +0800

    [hotfix] i fucked up again

--- apps/backend/src/shop-ranking/shop-ranking.cron.ts
+++ apps/backend/src/shop-ranking/shop-ranking.cron.ts
@@ -8,13 +8,11 @@ export class RankingScheduler {
   constructor(private shopRankingService: ShopRankingService) {}
 
   @Cron(CronExpression.EVERY_HOUR)
-  @Log({ logReturn: false })
   async updateHotRankings() {
     await this.shopRankingService.calculateAndUploadHotRankings();
   }
 
   @Cron(CronExpression.EVERY_DAY_AT_3AM)
-  @Log({ logReturn: false })
   async updateHomeRankings() {
     await this.shopRankingService.calculateAndUploadHomeRankings();
   }
