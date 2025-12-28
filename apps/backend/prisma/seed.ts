2dd2bc84c3da9ad6cb3b34c44081703059d32a5c
Andy <linandy40804@gmail.com>
2025-12-28 12:15:47 +0800

    [feat] update seed.ts and schools.json

--- apps/backend/prisma/seed.ts
+++ apps/backend/prisma/seed.ts
@@ -1,18 +1,19 @@
-import { PrismaClient } from "@prisma/client";
-import path from "path";
+import { PrismaClient } from '@prisma/client';
+import path from 'path';
 
 export const prisma = new PrismaClient();
 
 async function main() {
-  const schoolsJson = require(path.join(
-    process.cwd(),
-    "shared/jsons/schools.json"
-  ));
+  const schoolsJson = require(
+    path.join(process.cwd(), 'shared/jsons/schools.json'),
+  );
   const schools: any[] = schoolsJson.map((school: any) => ({
     abbreviation: school.abbreviation,
     emailFormats: school.emailFormat,
     name: school.name,
     studentIdFormat: school.studentIdFormat,
+    instagramAccount: school.instagramAccount,
+    websiteUrl: school.websiteUrl,
   }));
 
   for (const s of schools) {
