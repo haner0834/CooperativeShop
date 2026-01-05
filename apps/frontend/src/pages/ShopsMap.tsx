944ae41d06fc68c064af4d6ee37e5349c42ade84
Andy <linandy40804@gmail.com>
2026-01-06 02:54:32 +0800

    [feat] yeah. tired

--- apps/frontend/src/pages/ShopsMap.tsx
+++ apps/frontend/src/pages/ShopsMap.tsx
@@ -1,4 +1,4 @@
-import { Menu } from "lucide-react";
+import { Bookmark, Menu, Navigation, Search, ShoppingCart } from "lucide-react";
 import Sidebar from "../widgets/Sidebar";
 import { SidebarContent } from "../widgets/SidebarContent";
 import Logo from "@shared/app-icons/cooperativeshop-logo.svg?react";
@@ -56,23 +56,50 @@ const ShopsMap = () => {
 
   return (
     <div className="min-h-screen relative">
-      <button
-        onClick={() => setShowSidebar((prev) => !prev)}
-        className={`absolute top-4 left-4 btn ${
-          showSidebar || isDesktop ? "rounded-full" : "btn-circle"
-        } shadow-md z-50 transition-all flex gap-4`}
-      >
-        <Menu size={20} />
+      <div className="absolute inset-0 flex flex-col w-full">
+        <button
+          onClick={() => setShowSidebar((prev) => !prev)}
+          className={`m-4 mr-auto btn ${
+            showSidebar || isDesktop ? "rounded-full" : "btn-circle"
+          } shadow-md z-50 transition-all flex gap-4`}
+        >
+          <Menu size={20} />
+          {(showSidebar || isDesktop) && <Logo className="h-7 w-auto" />}
+        </button>
 
-        {(showSidebar || isDesktop) && <Logo className="h-7 w-auto" />}
-      </button>
+        <div className="mt-auto flex flex-col">
+          <div className="flex justify-between mx-4 items-end">
+            <button className="backdrop-blur-sm bg-base-100/25 p-[6px] rounded-full shadow-md z-10">
+              <ShoppingCart className="text-amber-500 p-2 bg-base-100/85 rounded-full h-9 w-9 overflow-visible" />
+            </button>
+            <div className="backdrop-blur-md z-10 shadow-md p-[6px] rounded-full h-auto flex flex-col bg-base-100/25 gap-[6px] transition-all">
+              {true && (
+                <button>
+                  <Bookmark className="p-2 bg-base-100/85 rounded-full h-9 w-9 overflow-visible" />
+                </button>
+              )}
+              <button>
+                <Navigation className="p-2 bg-base-100/85 rounded-full h-9 w-9 overflow-visible" />
+              </button>
+            </div>
+          </div>
+
+          <div className="m-4 mb-8 rounded-full p-2 backdrop-blur-sm bg-base-100/25 z-10 shadow-md">
+            <div className="input border-none rounded-full w-full h-8 bg-base-100 outline-none">
+              <Search size={18} />
+              <input type="text" placeholder="Search Shops" />
+            </div>
+          </div>
+        </div>
+      </div>
 
       <Sidebar isOpen={showSidebar} onClose={() => setShowSidebar(false)}>
         <SidebarContent />
       </Sidebar>
 
-      <div className="relative w-full h-screen overflow-clip">
+      <div className="relative w-full h-screen overflow-clip z-9">
         <PureMap />
+        {/* <div className="w-full h-full bg-accent"></div> */}
       </div>
     </div>
   );
