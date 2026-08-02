7934d9cc02ac979af8da42e89dbdd82ca0ddc311
Andy <linandy40804@gmail.com>
2026-08-03 03:04:16 +0800

    [feat] remove title, subtitle for patch dto

--- apps/backend/src/shop-draft/dto/patch-draft.dto.ts
+++ apps/backend/src/shop-draft/dto/patch-draft.dto.ts
@@ -4,8 +4,6 @@ import { ShopDraftDto } from './shop-draft.dto'; // 引入你原本的完整 DTO
 
 export class PatchShopDraftDto extends PartialType(
   PickType(ShopDraftDto, [
-    'title',
-    'subtitle',
     'address',
     'contactInfo',
     'description',
