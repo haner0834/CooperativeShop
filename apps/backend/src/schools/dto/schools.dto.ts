12b98d00e574e6e170600a4e04a0c37601d5f0ce
Andy <linandy40804@gmail.com>
2026-09-19 22:34:02 +0800

    [refactor] apply class-transformer

--- apps/backend/src/schools/dto/schools.dto.ts
+++ apps/backend/src/schools/dto/schools.dto.ts
@@ -1,10 +1,38 @@
-export interface SchoolDTO {
+import { Expose } from 'class-transformer';
+import { IsIn, IsNumber, IsOptional, IsString, IsUrl } from 'class-validator';
+
+export class SchoolDTO {
+  @IsString()
+  @Expose()
   id: string;
+
+  @IsString()
+  @Expose()
   name: string;
+
+  @IsString()
+  @Expose()
   abbreviation: string;
+
+  @IsIn(['google', 'credential'])
+  @Expose()
   loginMethod: string;
+
+  @IsString()
+  @IsOptional()
+  @Expose()
   instagramAccount: string | null;
+
+  @IsUrl()
+  @IsOptional()
+  @Expose()
   websiteUrl: string | null;
+
+  @IsNumber()
+  @Expose({ name: '_count.shops' })
   shopsCount: number;
+
+  @IsNumber()
+  @Expose({ name: '_count.users' })
   usersCount: number;
 }
