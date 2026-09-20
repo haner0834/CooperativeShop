958166015ecc9bcd45ef7fe3fddf1233b5a8c6dd
Andy <linandy40804@gmail.com>
2026-09-20 16:36:19 +0800

    [feat] migrate db

---
+++ apps/backend/prisma/migrations/20260920081807_add_icon_url_to_school/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "School" ADD COLUMN     "iconUrl" TEXT NOT NULL DEFAULT 'https://static.wikia.nocookie.net/adventures-of-chris-and-tifa/images/c/c1/ThisIsFineDog.jpeg/revision/latest?cb=20241003041905';
